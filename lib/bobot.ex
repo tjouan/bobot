03eba2ac06804786c9a297041c9d12de11df0122
Thibault Jouan <tj@a13.fr>
2014-08-07 16:50:46 +0000

    Join room

--- lib/bobot.ex
+++ lib/bobot.ex
@@ -9,13 +9,37 @@ defmodule Bobot do
       session = :exmpp_session.start
 
       host    = "im.a13.fr"
-      jid     = :exmpp_jid.make "bobot", host, "origin"
-      pass    = "MYPASSWORD"
+      jid     = :exmpp_jid.make "bobot", host, "electrons"
+      pass    = "oRdc3MzUxwzeoMEC"
 
       :exmpp_session.auth_basic_digest session, jid, String.to_char_list pass
       #:exmpp_session.connect_TCP session, ehost, 5222, starttls: :enabled
       :exmpp_session.connect_SSL session, String.to_char_list(host), 5223
       :exmpp_session.login session
+
+      status = :exmpp_presence.set_status :exmpp_presence.available(), ""
+      :exmpp_session.send_packet session, status
+
+
+      ##########################################################################
+
+      muc_host  = "muc.im.a13.fr"
+      muc_jid   = :exmpp_jid.make "arena", muc_host, "bobot"
+
+      packet = :exmpp_xml.append_child(
+        :exmpp_stanza.set_sender(
+          :exmpp_stanza.set_recipient(
+            :exmpp_presence.available(),
+            muc_jid
+          ),
+          jid
+        ),
+        :exmpp_xml.element('http://jabber.org/protocol/muc', 'x')
+      )
+
+      :exmpp_session.send_packet session, packet
+
+      :timer.sleep 8000
     end
   end
 end
