58d362f03971756d39821474c68e657c58c05754
Thibault Jouan <tj@a13.fr>
2015-04-12 18:09:56 +0000

    Build XMPP messages with delayed flag

--- lib/bobot/xmpp/message.ex
+++ lib/bobot/xmpp/message.ex
@@ -1,15 +1,17 @@
 defmodule Bobot.XMPP.Message do
+  alias Bobot.XMPP
   alias Bobot.XMPP.Message
   alias Bobot.XMPP.Packet
 
-  defstruct type: nil, from: nil, to: nil, body: nil
+  defstruct type: nil, from: nil, to: nil, body: nil, delayed: nil
 
 
   def build(packet) do
     %Message{
-      type: packet.attr,
-      from: packet.from,
-      body: Packet.message_body(packet)
+      type:     packet.attr,
+      from:     packet.from,
+      body:     Packet.message_body(packet),
+      delayed:  XMPP.msg_is_delayed(packet.raw)
     }
   end
 end
